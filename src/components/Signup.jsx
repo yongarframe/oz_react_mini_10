e1d6ab56892bcd15c1ee3e3e36354e319a34c444
yongarframe <yongar2002@gmail.com>
2025-05-08 22:52:37 +0900

    입력폼 기능 적용

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,57 +1,83 @@
+import { useState } from "react";
+
 export default function Signup() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [passWord, setPassWord] = useState("");
+  const [checkPassWord, setCheckPassWord] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log({
+      email,
+      name,
+      passWord,
+      checkPassWord,
+    });
+  };
+
   return (
     <div className="bg-gray-100 flex items-center justify-center min-h-screen">
       <div className="bg-white p-8 rounded-2xl shadow-md w-full max-w-md">
         <h2 className="text-2xl font-bold mb-6 text-center">회원가입</h2>
-        <form>
-          <div class="mb-4">
-            <label for="email" class="block text-sm font-medium mb-1">
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-sm font-medium mb-1">
               이메일
             </label>
             <input
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               type="email"
-              id="email"
               name="email"
-              class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
           </div>
           <div className="mb-4">
-            <label for="name" class="block text-sm font-medium mb-1">
+            <label htmlFor="name" className="block text-sm font-medium mb-1">
               이름
             </label>
             <input
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               type="text"
               id="name"
               name="name"
-              class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
           </div>
           <div className="mb-4">
-            <label for="password" class="block text-sm font-medium mb-1">
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium mb-1"
+            >
               비밀번호
             </label>
             <input
+              value={passWord}
+              onChange={(e) => setPassWord(e.target.value)}
               type="password"
               id="password"
               name="password"
-              class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
           </div>
           <div className="mb-6">
             <label
-              for="confirm-password"
-              class="block text-sm font-medium mb-1"
+              htmlFor="confirm-password"
+              className="block text-sm font-medium mb-1"
             >
               비밀번호 확인
             </label>
             <input
+              value={checkPassWord}
+              onChange={(e) => setCheckPassWord(e.target.value)}
               type="password"
               id="confirm-password"
               name="confirm-password"
-              class="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+              className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
               required
             />
           </div>
